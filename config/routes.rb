3deaab45bea84978ef8ddaa5189ed97573febdbb
Roberto Quesada <robertoquesadalizana@gmail.com>
2015-12-04 09:37:39 +0100

    Changes on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/:player_id' => 'matches#show_matches_player'
   get '/matches/:faction' => 'matches#show_faction_results'
+  get '/players/:id' => 'players#show'
 end
